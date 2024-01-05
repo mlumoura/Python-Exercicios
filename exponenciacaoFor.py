712b1a0b544243d7711e7487a48ac6a467009308
mlumoura <mlumoura@gmail.com>
2024-01-04 21:56:34 -0300

    Others exercises

---
+++ exponenciacaoFor.py
@@ -0,0 +1,4 @@
+power = 1
+for expo in range(16):
+  print("2 à potência de", expo, "é", power)
+  power *= 2
---
+++ upper.py
@@ -0,0 +1,10 @@
+# Solicita que o usuário insira uma palavra
+# e atribua-a à variável user_word.
+user_word = input("Digite ua palavra: ")
+
+user_word = user_word.upper()
+
+for letter in user_word:
+    # Preenchao corpo do loop for.
+    if letter !="A"and letter !="E" and letter !="I" and letter !="O" and letter !="U":
+        print(letter)
